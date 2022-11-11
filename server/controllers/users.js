a8d4ade30057c585862c2ae272080dfcde2fdf76
Findlay20 <cameron.findlay@hotmail.co.uk>
2022-11-11 12:13:59 +0000

    Moved login/register functions to users controller

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,7 +1,6 @@
-const express = require('express');
-const router = express.Router();
+const User = require('../models/user');
 
-router.post('/', async (req, res) => {
+async function login () {
     try {
         const user = await User.findByUsername(req.body.username)
         console.log(user)
@@ -24,4 +23,16 @@ router.post('/', async (req, res) => {
     } catch (err) {
         res.status(401).json({ err });
     }
-}) 
+};
+
+async function register () {
+    try {
+
+
+        
+    } catch (err) {
+        res.status(401).json({ err });
+    }
+};
+
+module.exports = { login, register}
--- server/server.js
+++ server/server.js
@@ -5,10 +5,9 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
-const loginRoute = require('./routes/login');
-const registerRoute = require('./routes/register');
+const userController = require('./controllers/users')
 
-server.use('/login', loginRoute);
-server.use('/register', registerRoute);
+server.post('/login', userController.login);
+server.post('/register', userController.register);
 
 server.get('/', (req, res) => res.send('Accessing HabitHelper backend'));
