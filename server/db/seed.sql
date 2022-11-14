2750e2884e56e75528faa7398528af67e1942a21
Findlay20 <cameron.findlay@hotmail.co.uk>
2022-11-14 16:45:05 +0000

    Added habits table to database

--- server/db/seed.sql
+++ server/db/seed.sql
@@ -1,3 +1,9 @@
 INSERT INTO users (email, firstName, lastName, username, password)
 VALUES
 ('dockerdemon@gmail.com', 'Demon', 'Docker', 'dockerdemon', '$2b$12$4Ah04EzYN3ilHi4b7ptGCuL2DC/bucfo9fV9ZvFPldCV7SAGaJhOm');
+
+INSERT INTO habits (habit, recommended_daily_goal, recommended_weekly_goal)
+VALUES
+('Sleep', '8 hours', '56 hours'),
+('Exercise', '30 minutes', '3 hours'),
+('Water', '3 litres', '56 litres');
