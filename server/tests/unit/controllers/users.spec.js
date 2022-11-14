1ccca4173fb101e1c40d04d9d28cb9f69a8c526b
Findlay20 <cameron.findlay@hotmail.co.uk>
2022-11-14 15:37:12 +0000

    Working on tests

--- server/tests/unit/controllers/users.spec.js
+++ server/tests/unit/controllers/users.spec.js
@@ -13,7 +13,7 @@ describe('Users controller', () => {
 
     describe('index', () => {
         test('it returns all users with a 200 status code', async () => {
-            let testUsers = ['user1', 'uesr2']
+            const testUsers = ['user1', 'uesr2']
             jest.spyOn(User, 'all', 'get')
                  .mockResolvedValue(testUsers);
             await usersController.index(null, mockRes);
@@ -23,20 +23,20 @@ describe('Users controller', () => {
     });
 
     describe('login', () => {
-        test('', async () => {
-            let testUser = {username: "testusername", password: "testpass"}
+        test('allows login with 200 status code', async () => {
+            const testUser = {username: "testusername", password: "testpass"}
             jest.spyOn(User, 'findByUsername')
                 .mockResolvedValue(User.findByUsername(testUser))
             const mockReq = {body: testUser}
             await usersController.login(mockReq, mockRes)
             expect(mockStatus).toHaveBeenCalledWith(200)
-            expect(mockJson).toHaveBeenCalledWith({success: true , token: ""})
+            expect(mockJson).toHaveBeenCalledWith({success: true , token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiZXRzdCIsImZpcnN0TmFtZSI6ImFkc2YiLCJpYXQiOjE2Njg0Mzg0NjUsImV4cCI6MTY2ODQ0MjA2NX0.srKjIH_mWogqk2AcOGzXRuwAlYSIodE8Uccp7PPXw20"})
         })
     })
 
     describe('register', () => {
         test('new user created with a 201 status code', async () => {
-            let newUser = {id: 1, username: "testusername", firstName: "Test", lastName: "Tester", password: "testpass"}
+            const newUser = {username: "testusername", firstName: "Test", lastName: "Tester", password: "testpass"}
             jest.spyOn(User, 'create')
                 .mockResolvedValue(new User(newUser))
             const mockReq = {body: newUser}
@@ -47,6 +47,11 @@ describe('Users controller', () => {
     })
 
     describe('createToken', () => {
-
+        test('token is created', async () => {
+            const testUserData = {username: "testusername", firstName:"Test"}
+            const testToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiZXRzdCIsImZpcnN0TmFtZSI6ImFkc2YiLCJpYXQiOjE2Njg0Mzg0NjUsImV4cCI6MTY2ODQ0MjA2NX0.srKjIH_mWogqk2AcOGzXRuwAlYSIodE8Uccp7PPXw20"
+            const generatedToken = await usersController.createToken(testUserData)
+            expect(generatedToken).toEqual(testToken)
+        })
     })
 })
