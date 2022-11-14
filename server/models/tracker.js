6762ee5f5604c2831a19d4167166fdfd5a86678c
Cameron Findlay <cameron.findlay@hotmail.co.uk>
2022-11-14 20:57:00 +0000

    Fixed error with Tracker constructor

--- server/models/tracker.js
+++ server/models/tracker.js
@@ -3,19 +3,20 @@ const {db} = require('../db/init');
 module.exports = class Tracker {
     constructor(data){
         this.id = data.id
-        this.habit_id = data.habitId
+        this.habitId = data.habit_id
         this.dailyValue = data.dailyValue
         this.date = data.date
-        this.userId = data.userId
+        this.userId = data.user_id
     }
 
     static create ({ habitId, dailyValue, date, userId}){
         return new Promise(async (res, rej) => {
             try {
-                let result = await db.query(`INSERT INTO tracker (id, havitId, dailyValue, date, userId)
+                let result = await db.query(`INSERT INTO tracker (habit_id, habit_daily_value, date, user_id)
                                                 VALUES ($1, $2, $3, $4) RETURNING *;`,[habitId, dailyValue, date, userId]);
                 let tracker = new Tracker(result.rows[0]);
-                res(user)
+                console.log(tracker)
+                res(tracker)
             } catch (err) {
                 rej(`Error creating user: ${err}`)
             }
