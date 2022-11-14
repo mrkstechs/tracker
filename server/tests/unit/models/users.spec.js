293f51b8fa2a487e44ae3db07f05d1fb97cfbaa3
Findlay20 <cameron.findlay@hotmail.co.uk>
2022-11-14 14:29:52 +0000

    Added some tests

--- server/tests/unit/models/users.spec.js
+++ server/tests/unit/models/users.spec.js
@@ -9,11 +9,31 @@ describe('User', () => {
 
     afterAll(() => jest.resetAllMocks())
 
+    describe('Get all users', () => {
+        test('it resolves with all users on successful db query', async () => {
+            jest.spyOn(db, 'query')
+                .mockResolvedValueOnce({ rows: [{}, {}, {}]});
+            const all = await User.all;
+            expect(all).toHaveLength(3)
+        })
+    });
+
+    describe('create', () => {
+        test('it resolves with new user on successful db query', async () => {
+            let userData = { username: 'newUser', firstName: "New", lastName: "User", email: "newuser@test.com", password:"testpass" }
+            jest.spyOn(db, 'query')
+                .mockResolvedValueOnce({rows: [ { ...userData, id: 1 }] });
+            const result = await User.create(userData);
+            expect(result).toHaveProperty('id')
+        })
+    });
+    
+
     describe('findByUsername', () => {
         test('it resolves with user on successful db query', async () => {
             let userData = { id: 1, username: 'testusername' }
             jest.spyOn(db, 'query')
-                .mockResolvedValueOnce({rows: [ ownerData] });
+                .mockResolvedValueOnce({rows: [ userData ] });
             const result = await User.findByUsername("testusername");
             expect(result).toBeInstanceOf(User)
         })
