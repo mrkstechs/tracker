07e596bf8cf1f52614aba5816ef4b98a95d379aa
Findlay20 <cameron.findlay@hotmail.co.uk>
2022-11-14 14:54:28 +0000

    Working on tests

--- server/tests/unit/controllers/users.spec.js
+++ server/tests/unit/controllers/users.spec.js
@@ -11,5 +11,30 @@ describe('Users controller', () => {
 
     afterAll(() => jest.resetAllMocks());
 
-    
+    describe('index', () => {
+        test('it returns all users with a 200 status code', async () => {
+            let testUsers = ['user1', 'uesr2']
+            jest.spyOn(User, 'all', 'get')
+                 .mockResolvedValue(testUsers);
+            await usersController.index(null, mockRes);
+            expect(mockStatus).toHaveBeenCalledWith(200);
+            expect(mockJson).toHaveBeenCalledWith(testUsers);
+        })
+    });
+
+    describe('login', () => {
+        test('', async () => {
+
+        })
+    })
+
+    describe('register', () => {
+        test('new user created with a 201 status code', async () => {
+            let newUser = {id: 1, username: "testusername", }
+        })
+    })
+
+    describe('createToken', () => {
+
+    })
 })
