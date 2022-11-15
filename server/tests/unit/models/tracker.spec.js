e605533db821067bb4c16a87a7db499bcf71bc9d
Findlay20 <cameron.findlay@hotmail.co.uk>
2022-11-15 11:19:05 +0000

    Testing of tracker model done

--- server/tests/unit/models/tracker.spec.js
+++ server/tests/unit/models/tracker.spec.js
@@ -31,21 +31,21 @@ describe('Tracker', () => {
 
     describe('findByUserId', () => {
         test('it resolves with trackers of a user on successful db query', async () => {
-            let trackerData = [{habit_id: 1, dailyValue: 2, date: "2022-11-15", userId: "1"},{habitId: 2, dailyValue: 2, date: "2022-11-15", userId: "1"},{habitId: 1, dailyValue: 2, date: "2022-11-15", userId: "2"}]
+            let trackerData = {id: 1, habit_id: 1, dailyValue: 2, date: "2022-11-15", userId: "1"}
             jest.spyOn(db, 'query')
                 .mockResolvedValueOnce({rows: [ trackerData ] });
             const result = await Tracker.findByUserId(1);
-            expect(result).toHaveLength(2)
+            expect(result[0]).toBeInstanceOf(Tracker)
         })
     });
 
     describe('findByUserAndHabit', () => {
         test('it resolves with specific trackers of a user on successful db query', async () => {
-            let trackerData = [{habitId: 1, dailyValue: 2, date: "2022-11-15", userId: "1"},{habitId: 2, dailyValue: 2, date: "2022-11-15", userId: "1"},{habitId: 2, dailyValue: 2, date: "2022-11-16", userId: "1"},{habitId: 1, dailyValue: 2, date: "2022-11-15", userId: "2"}]
+            let trackerData = {id:1, habitId: 2, dailyValue: 2, date: "2022-11-15", userId: "1"}
             jest.spyOn(db, 'query')
                 .mockResolvedValueOnce({rows: [ trackerData ] });
             const result = await Tracker.findByUserAndHabit(1,2);
-            expect(result).toHaveLength(2)
+            expect(result[0]).toBeInstanceOf(Tracker)
         })
     });
 })
