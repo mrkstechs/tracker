3470d13a2627458b2d3f6033693c524bce75906b
Findlay20 <cameron.findlay@hotmail.co.uk>
2022-11-16 17:07:46 +0000

    Add goal form now displays (not functional) when user isn't tracking sleep

--- client/assets/js/sleep.js
+++ client/assets/js/sleep.js
@@ -2,7 +2,7 @@ window.addEventListener('load', createDisplay)
 
 async function createDisplay() {
     const { user, sleepGoal, sleepTrackers, lastSleep } = await retrieveSleepData()
-    if (!sleepGoal){displayAddSleepGoal} else {
+    if (!sleepGoal){displayNewGoalForm(user)} else {
         displayLatestSleep(sleepGoal, lastSleep)
         displayGoal(sleepGoal)
         displayStreak(sleepTrackers, sleepGoal)
@@ -11,15 +11,46 @@ async function createDisplay() {
 }
 
 async function retrieveSleepData() {
+    try {
     user = await JSON.parse(localStorage.getItem("user"));
     const sleepGoal = (await (await fetch(`http://localhost:3000/goals/${user.id}/1`)).json())[0]
     const sleepTrackers = await (await fetch(`http://localhost:3000/trackers/${user.id}/1`)).json()
     const lastSleep = sleepTrackers[(sleepTrackers.length)-1]
     return  { user, sleepGoal, sleepTrackers, lastSleep };
+    } catch (err) {
+        return false
+    }
 }
 
-function displayAddSleepGoal () {
-    const progressSection = document.querySelector('div.progress')
+// function displayAddSleepGoal (user) {
+//     const trackersSection = document.querySelector('div.trackers')
+    // const markup = `<div class="habit" id="sleepHabit">
+    //                     <i class="bi bi-alarm"></i>
+    //                     <h2>Sleep</h2>
+    //                     <div class="newGoal">
+    //                         <h1>+</h1>
+    //                         <h4>Start tracking sleep!</h4>
+    //                     </div>
+    //                 </div>`
+//     trackersSection.insertAdjacentHTML('afterbegin', markup)
+//     newGoalButton = document.querySelector('div.newGoal')
+//     newGoalButton.addEventListener('click', (user) => {displayNewGoalForm(user)})
+// }
+
+function displayNewGoalForm (user) {
+    const goalSection = document.querySelector('div.trackers')
+    goalSection.innerHTML = "";
+    const goalForm = `<form id="sleepForm">
+                        <input type="range" id="addSleep" name="addSleep" value="0" min="0" max="12" step="1" oninput="this.nextElementSibling.firstChild.value = this.value"></input>
+                        <label for="addSleep"><output>0</output> hours</label>
+                        <input type="submit" id="submitGoal" value="Add Goal!">
+                    </form>
+                    <button id="backToHome">back</button>`
+    goalSection.insertAdjacentHTML('afterbegin', goalForm)
+
+    const button = document.querySelector('#backToHome')
+    button.addEventListener('click', () => {window.location.assign('/client/homepage.html')})
+
 }
 
 function displayLatestSleep(sleepGoal, lastSleep) {
