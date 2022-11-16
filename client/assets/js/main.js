03f63ad2e48194a3a5bad387042f28b247e61750
DayoTitiloye-code <DayoTitiloye@outlook.com>
2022-11-16 14:36:16 +0000

    Commit homepage before pull

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -1,22 +1,10 @@
-const scrollContainer = document.querySelector(".cards");
-const title = document.querySelector('#profileheader');
-const home = document.querySelector('#home')
-firstName = localStorage.getItem('firstName');
-title.textContent = `Hello ${firstName} welcome to your profile`;
+// const scrollContainer = document.querySelector(".cards");
+// const title = document.querySelector('#profileheader');
+// const home = document.querySelector('#home')
 
 
-home.addEventListener('click', showHome);
+// home.addEventListener('click', showHome);
 
-scrollContainer.addEventListener("wheel", (event) => {
-    event.preventDefault();
-
-    scrollContainer.scrollBy({
-        left: event.deltaY < 0 ? -50 : 50,
-    });
-    
-
-});
-
-function showHome(){
-    window.location.assign('/client/homepage.html')
-}
+// function showHome(){
+//     window.location.assign('/client/homepage.html')
+// }
