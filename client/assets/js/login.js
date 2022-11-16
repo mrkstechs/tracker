64cd85c0cecbc1aa1a8a2ee1ef461cab74f85a45
Findlay20 <cameron.findlay@hotmail.co.uk>
2022-11-16 16:20:43 +0000

    Removing old code

--- client/assets/js/login.js
+++ client/assets/js/login.js
@@ -112,8 +112,4 @@ function login(data){
     localStorage.setItem('token', data.token);
     
     window.location.assign("/client/homepage.html");
-    
-    // const message = document.createElement('p');
-    // message.textContent = "Succesful login";
-    // form.append(message);
 }
