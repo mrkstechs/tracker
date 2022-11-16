676e0ca5cb307cf4a772b24d2fe86ae8c980186d
Findlay20 <cameron.findlay@hotmail.co.uk>
2022-11-16 17:42:55 +0000

    New user can now add water goal

--- client/assets/js/water.js
+++ client/assets/js/water.js
@@ -2,9 +2,17 @@ window.addEventListener('load', createDisplay)
 
 async function createDisplay() {
     const { user, waterGoal, waterTracker, lastDrink } = await retrieveWaterData()
-    displayGoal(waterGoal)
-    displayWaterProgress(waterGoal, lastDrink)
-    displayStreak(waterTracker, waterGoal)
+    if (!waterGoal) {displayNewGoalForm(user)} 
+        else if (!lastDrink) {
+            displayGoal(waterGoal)
+            displayStreak(waterTracker, waterGoal)
+            displayAddTrackerButton(user)
+        } else {
+            displayGoal(waterGoal)
+            displayWaterProgress(waterGoal, lastDrink)
+            displayStreak(waterTracker, waterGoal)
+            displayAddTrackerButton(user)
+        }
 }
 
 async function retrieveWaterData(){
@@ -12,11 +20,57 @@ async function retrieveWaterData(){
     const waterGoal = (await (await fetch(`http://localhost:3000/goals/${user.id}/3`)).json())[0]
     const waterTracker = await (await fetch(`http://localhost:3000/trackers/${user.id}/3`)).json()
     const lastDrink = waterTracker[(waterTracker.length) - 1]
-    //console.log(user)
-    console.log(waterGoal)
-    // console.log(waterTracker)
-    console.log(lastDrink)
     return { user, waterGoal, waterTracker, lastDrink};
+}
+
+function displayNewGoalForm () {
+    const goalSection = document.querySelector('div.trackers')
+    goalSection.innerHTML = "";
+    const goalForm = `<form id="goalForm">
+                        <input type="range" id="addGoal" name="addGoal" value="0" min="0" max="20" step="1" oninput="this.nextElementSibling.firstChild.value = this.value"></input>
+                        <label for="addGoal"><output>0</output> cups</label>
+                        <input type="submit" id="submitGoal" value="Add Goal!">
+                    </form>
+                    <button id="backToHome">back</button>`
+    goalSection.insertAdjacentHTML('afterbegin', goalForm)
+
+    const button = document.querySelector('#backToHome')
+    button.addEventListener('click', () => {window.location.assign('/client/homepage.html')})
+
+    const newGoalForm = document.querySelector('#goalForm')
+    newGoalForm.addEventListener('submit', submitGoal)
+}
+
+async function submitGoal (e) {
+    e.preventDefault;
+    const dailyGoal = e.target.addGoal.value
+    try {
+        const body = { "userId": user.id, 
+                        "habitId": 3, 
+                        "dailyGoal": dailyGoal, 
+                        "weeklyGoal":  dailyGoal*7,
+                        "goalUnits": "cups"}
+        const options = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(body)
+        }
+        const r = await fetch(`http://localhost:3000/goals`, options)
+        const data = await r.json()
+        if (data.err){ throw Error(data.err) }
+        window.location.reload();
+    } catch (err) {
+        console.warn(err);
+    }
+}
+
+function displayAddTrackerButton (user) {
+    const logWaterSection = document.querySelector('div.logWater')
+    logWaterSection.innerHTML = ""
+    markup = `<button id="displayAddTrackerForm" class="sleepButton"><i class="bi-plus"></i><br>Log Water!</button>`
+    logWaterSection.insertAdjacentHTML('afterbegin',markup)
+    const button = document.querySelector('#displayAddTrackerForm')
+    button.addEventListener('click', () => {displayAddTrackerForm(logWaterSection, user)})
 }
 
 async function displayGoal(waterGoal){
