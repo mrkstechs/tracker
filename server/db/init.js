37e34daadcfd08edbfa0a04df644b6e9285b6c66
Kelechi Okoli <kelechio1992@gmail.com>
2022-11-16 17:16:28 +0000

    changed sql connection options & init data

--- server/db/init.js
+++ server/db/init.js
@@ -1,7 +1,9 @@
 const { Client } = require("pg");
 
 const db = new Client({
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
+    host: process.env.PGHOST,
+    ssl: true
 })
 
 db.connect(err => err ? console.error('connection error', err.stack) : console.log('postgres db conntected!'))
