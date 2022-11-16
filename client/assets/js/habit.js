bf769c26365565ef8b4529180a9f8b8950126df9
Arqestra <mshawdietrich@gmail.com>
2022-11-16 09:23:55 +0000

    Pulling

--- client/assets/js/habit.js
+++ client/assets/js/habit.js
@@ -3,12 +3,40 @@ async function exPreview (user) {
     try {
         const response = await fetch(`http://localhost:3000/goals/${user}`)
         const data = await response.json();
-        const execiseGoal = data.filter(obj => obj.habitId == 2)
+        const exerciseGoal = data.filter(obj => obj.habitId == 2)
         const recommendedGoals = await fetch('http://localhost:3000/habits').then(data => data.json()).then(goals => goals.find(habit => habit.id == 2))
-        console.log(`Users current goal/progress:`, execiseGoal)
+        console.log(`User's current goal/progress:`, exerciseGoal)
         console.log(`Recommended goal/progress:`, recommendedGoals)
     } catch (err) {
         console.warn(err);
+    }
+}
+
+function getUserGoals(user) {
+    fetch(`http://localhost:3000/goals/${user}`)
+    .then(r => r.json())
+    .then(showExercise)
+    .catch(console.warn)
+}
+
+function showExercise () {
+    const goal = document.querySelector('#exercise')
+    let goalNumber = document.createElement('h4')
+    goalNumber.id = "goalnumber"
+    goal.append(goalNumber) 
+}
+
+
+
+
+
+
+
+async function slePreview (user) {
+    try {
+        const response = await fetch(`http://localhost:3000/goals/${user}`)
+    } catch {
+
     }
 }
 
@@ -21,3 +49,7 @@ async function exPreview (user) {
 // }
 
 exPreview(1)
+
+slePreview()
+
+watPreview()
