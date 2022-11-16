e940c4218728603516342444f90157af149566be
Kelechi Okoli <kelechio1992@gmail.com>
2022-11-16 17:32:07 +0000

    changed sql connection options & init data

--- server/db/init.js
+++ server/db/init.js
@@ -1,11 +1,7 @@
 const { Client } = require("pg");
 
 const db = new Client({
-    connectionString: process.env.DATABASE_URL,
-    host: process.env.PGHOST,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
+    connectionString: 'postgres://xwxkepfzogadhh:17e6276c5dd824a3a3c1e1e57ce566c5324993b0f5a8f5cfe571b86fdbdd44a8@ec2-34-252-216-149.eu-west-1.compute.amazonaws.com:5432/db9ccbldikovr1',
     ssl: true
 })
 console.log(process.env.PGHOST, process.env.PORT)
