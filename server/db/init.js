442fc16945f841dde7cb7acc9cecbb1b03df64d5
Kelechi Okoli <kelechio1992@gmail.com>
2022-11-16 17:43:12 +0000

    changed sql connection options & init data

--- server/db/init.js
+++ server/db/init.js
@@ -1,7 +1,7 @@
 const { Client } = require("pg");
 
 const db = new Client({
-    connectionString: 'postgres://xwxkepfzogadhh:17e6276c5dd824a3a3c1e1e57ce566c5324993b0f5a8f5cfe571b86fdbdd44a8@ec2-34-252-216-149.eu-west-1.compute.amazonaws.com:5432/db9ccbldikovr1',
+    connectionString: 'postgres://fnzorbgwbnijwh:2b288c3af4c52614d1fc2eb35368543628cfe6819c61091f153a6c5a4129ca9a@ec2-54-228-218-84.eu-west-1.compute.amazonaws.com:5432/d23rs2o3k32p1f',
 })
 db.connect(err => err ? console.error('connection error!', err.stack) : console.log('postgres db conntected!'))
 
