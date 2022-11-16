008408aae851b6e131e11114144cee3e01bea008
Findlay20 <cameron.findlay@hotmail.co.uk>
2022-11-16 16:34:05 +0000

    Displays start tracking sleep message if no current goal

--- client/assets/js/homepage.js
+++ client/assets/js/homepage.js
@@ -1,30 +1,30 @@
-const sleepButton = document.querySelector('#sleepButton');
-const exericseButton = document.querySelector('#exerciseButton');
-const waterButton = document.querySelector('#waterButton');
-const profileButton = document.querySelector('#profileButton');
+// const sleepButton = document.querySelector('#sleepButton');
+// const exericseButton = document.querySelector('#exerciseButton');
+// const waterButton = document.querySelector('#waterButton');
+// const profileButton = document.querySelector('#profileButton');
 
 
-sleepButton.addEventListener('click', showSleep);
-waterButton.addEventListener('click', showWater);
-exerciseButton.addEventListener('click', showExercise);
-profileButton.addEventListener('click', showProfile);
+// sleepButton.addEventListener('click', showSleep);
+// waterButton.addEventListener('click', showWater);
+// exerciseButton.addEventListener('click', showExercise);
+// profileButton.addEventListener('click', showProfile);
 
 
 
-function showSleep(){
-    window.location.assign("/client/sleep.html")
-}
+// function showSleep(){
+//     window.location.assign("/client/sleep.html")
+// }
 
-function showWater(){
-    window.location.assign("/client/water.html")
-}
+// function showWater(){
+//     window.location.assign("/client/water.html")
+// }
 
-function showExercise(){
-    window.location.assign("/client/exercise.html")
-}
+// function showExercise(){
+//     window.location.assign("/client/exercise.html")
+// }
 
-function showProfile(){
-    window.location.assign("/client/profilepage.html");
-}
+// function showProfile(){
+//     window.location.assign("/client/profilepage.html");
+// }
 
-module.exports = { showExercise, showSleep, showWater, showProfile}
+// module.exports = { showExercise, showSleep, showWater, showProfile}
