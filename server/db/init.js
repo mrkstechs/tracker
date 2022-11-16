e4fbf776ac997cb982817ab70e658db65b2f5c40
Kelechi Okoli <kelechio1992@gmail.com>
2022-11-16 14:40:40 +0000

    added package and config to get db working in production

--- server/db/init.js
+++ server/db/init.js
@@ -5,6 +5,8 @@ const config = require('../../knexfile')
 
 const dbConnect = knex(config.production)
 
-const db = new Pool();
+const db = new Pool({
+    connectionString: process.env.DATABASE_URL
+});
 
 module.exports = {db, dbConnect};
