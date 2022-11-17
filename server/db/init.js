1e6f9338e606d4bfb50a6e6088a9aaa017372b3c
Kelechi Okoli <kelechio1992@gmail.com>
2022-11-17 10:59:21 +0000

    edited db connection file

--- server/db/init.js
+++ server/db/init.js
@@ -1,11 +1,11 @@
 const { Client } = require("pg");
 
 const db = new Client({
-    // connectionString: process.env.DATABASE_URL,
-    host: process.env.PGHOST,
-    password: process.env.PGPASSWORD,
-    user: process.env.PGUSER,
-    database: process.env.PGDATABASE
+    connectionString: process.env.DATABASE_URL,
+    // host: process.env.PGHOST,
+    // password: process.env.PGPASSWORD,
+    // user: process.env.PGUSER,
+    // database: process.env.PGDATABASE
 })
 
 db.connect(err => err ? console.error('connection error!', err.stack) : console.log('postgres db conntected!'))
