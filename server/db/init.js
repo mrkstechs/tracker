4ac1c8aac6472660203b5af0fcf3f5621facd747
Kelechi Okoli <kelechio1992@gmail.com>
2022-11-17 10:53:09 +0000

    added ENV vars to talk to pg via heroku

--- server/db/init.js
+++ server/db/init.js
@@ -2,10 +2,12 @@ const { Client } = require("pg");
 
 const db = new Client({
     connectionString: process.env.DATABASE_URL,
-    ssl: true
+    host: process.env.PGHOST,
+    password: process.env.PGPASSWORD,
+    user: process.env.PGUSER,
+    database: process.env.PGDATABASE
 })
 
-console.log(process.env.DATABASE_URL)
 db.connect(err => err ? console.error('connection error!', err.stack) : console.log('postgres db conntected!'))
 
 module.exports = {db};
