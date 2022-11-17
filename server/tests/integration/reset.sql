421cac8d46aeac57d2b9b3622d50ca051c0df657
Findlay20 <cameron.findlay@hotmail.co.uk>
2022-11-17 11:55:13 +0000

    All user routes tested

--- server/tests/integration/reset.sql
+++ server/tests/integration/reset.sql
@@ -2,5 +2,5 @@ TRUNCATE users, habits, goals, tracker RESTART IDENTITY;
 
 INSERT INTO users (email, firstName, lastName, username, password)
 VALUES
-('test1@gmail.com', 'Test1', 'Tester', 'test1', '$2b$12$WBnUyP4wl83las2hJtiikOQ6OmoRA2UwJ9.PtW1uv62CSJYMsVrs.'),
-('test2@gmail.com', 'Test2', 'Tester', 'test2', '');
+('test1@gmail.com', 'Test1', 'Tester', 'test1', '$2b$12$/UqRatUKrAkGrduQp5XmP.I3Gb6szpR/V/lAHiZSZ4c/GyNbmtZMW'),
+('test2@gmail.com', 'Test2', 'Tester', 'test2', '$2b$12$/UqRatUKrAkGrduQp5XmP.I3Gb6szpR/V/lAHiZSZ4c/GyNbmtZMW');
--- server/tests/integration/userEndpoints.spec.js
+++ server/tests/integration/userEndpoints.spec.js
@@ -20,4 +20,19 @@ describe('User endpoints', () => {
         expect(res.body[1].username).toEqual('test2')
     });
 
+    it('should allow login with correct details', async () => {
+        const res = await request(api).post('/users/login').send({"username": "test1", "password": "abc123"})
+        expect(res.statusCode).toEqual(200)
+    });
+
+    it('should allow registeration', async () => {
+        const res = await request(api).post('/users/register').send({"email": "test@gmail.com",
+                                                                    "firstName": "Test",
+                                                                    "lastName": "Tester",
+                                                                    "username": "testuser",
+                                                                    "password": "$2b$12$4Ah04EzYN3ilHi4b7ptGCuL2DC/bucfo9fV9ZvFPldCV7SAGaJhOm"
+                                                                     })
+        expect(res.statusCode).toEqual(201)
+    });
+
 })
