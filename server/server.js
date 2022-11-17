e0977079e581a5a3d2ff61fd441645c25c18a691
Kelechi Okoli <kelechio1992@gmail.com>
2022-11-17 12:20:18 +0000

    seed db on init

--- server/server.js
+++ server/server.js
@@ -6,11 +6,15 @@ server.use(cors());
 server.use(express.json());
 
 const apiRoutes =  require('./routes/api')
+const port = process.env.PORT || 3000;
 
 server.use(express.static(path.resolve('./client/')))
 server.use('/api', apiRoutes)
 
 
 server.get('/', (req, res) => res.send('Accessing HabitHelper backend'))
+
+
+app.listen(port, () => console.log(`Express now departing from port ${port}!`))
 
 module.exports = server;
