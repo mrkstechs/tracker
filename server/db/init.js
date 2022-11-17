400a408ad8b1c9b2a311e054426d934131d643b1
Kelechi Okoli <kelechio1992@gmail.com>
2022-11-17 10:44:58 +0000

    moved package.json file to project root

--- server/db/init.js
+++ server/db/init.js
@@ -2,7 +2,10 @@ const { Client } = require("pg");
 
 const db = new Client({
     connectionString: process.env.DATABASE_URL,
+    ssl: true
 })
+
+console.log(process.env.DATABASE_URL)
 db.connect(err => err ? console.error('connection error!', err.stack) : console.log('postgres db conntected!'))
 
 module.exports = {db};
